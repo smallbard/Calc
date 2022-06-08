b7422d574dce4647326d94bfdcda5a952c474ac7
Olivier Le Guyader <olivier.leguyader@alcuin.com>
2022-06-08 12:56:08 +0200

    Console program

--- Calc.Launcher/Program.cs
+++ Calc.Launcher/Program.cs
@@ -6,8 +6,17 @@ namespace Calc.Launcher
     {
         static void Main(string[] args)
         {
-            var tokenizer = new Tokenizer();
-            
+            if (args.Length == 0)
+            {
+                Console.WriteLine("You must provide a command line argument.");
+                return;
+            }
+
+            var interpreter = new Interpreter();
+            var ast = new Parser().Parse(new Tokenizer().Tokenize(args[0]));
+            ast.Accept(interpreter);
+            Console.WriteLine(interpreter.Result);
+
         }
     }
 }
